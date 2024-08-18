3cbc5d976711d5a085d72d0254d7c8afb991c9a1
jaihernandezme <160196595+jaihernandezme@users.noreply.github.com>
2024-08-18 09:41:44 -0500

    Add files via upload

---
+++ PersonaEj81.java
@@ -0,0 +1,14 @@
+package Personas;
+
+public class PersonaEj81 {
+    String nombre;
+    String apellidos; 
+    String telefono;
+    String direccion;
+public PersonaEj81(String nombre, String apellidos, String telefono, String dirección) {
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.telefono = telefono;
+    this.direccion = dirección;
+}
+}
