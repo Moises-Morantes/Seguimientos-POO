dca36f2cf74e0ee1cf3be107c8b7d5b9bcbfd41a
Moises Morantes Gonzalez <141855529+Moises-Morantes@users.noreply.github.com>
2024-08-18 20:43:50 -0500

    4.1 and 4.2
    r

---
+++ 4.1_and_4.2/4.1/CuentaAhorros.java
@@ -0,0 +1,47 @@
+package cuenta;
+
+public class CuentaAhorros extends Cuenta {
+
+    private boolean activa;
+
+    public CuentaAhorros(float saldo, float tasa) {
+        super(saldo, tasa);
+        if (saldo < 10000) 
+            activa = false;
+        else
+            activa = true;
+    }
+
+    @Override
+    public void retirar(float cantidad) {
+        if (activa) {
+            super.retirar(cantidad);
+        }
+    }
+
+    @Override
+    public void consignar(float cantidad) {
+        if (activa) {
+            super.consignar(cantidad);
+        }
+    }
+
+    @Override
+    public void extractoMensual() {
+        if (númeroRetiros > 4) {
+            comisiónMensual += (númeroRetiros - 4) * 1000;
+        }
+        super.extractoMensual();
+        if (saldo < 10000) {
+            activa = false;
+        }
+    }
+
+    @Override
+    public void imprimir() {
+        System.out.println("Saldo = $" + saldo);
+        System.out.println("Comisión mensual = $" + comisiónMensual);
+        System.out.println("Número de transacciones = " + (númeroConsignaciones + númeroRetiros));
+        System.out.println();
+    }
+}
---
+++ 4.1_and_4.2/4.2/Apartamento.java
@@ -0,0 +1,16 @@
+package inmuebles;
+
+public class Apartamento extends InmuebleVivienda {
+public Apartamento(int identificadorInmobiliario, int área, String 
+dirección, int númeroHabitaciones, int númeroBaños) {
+
+super(identificadorInmobiliario, área, dirección, 
+númeroHabitaciones, númeroBaños);
+}
+
+void imprimir() {
+super.imprimir(); 
+}
+}
+
+
